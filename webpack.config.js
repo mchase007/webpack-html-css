4e145b61915020c6c6c75b45dbad416fc75c365f
unknown <>
2020-07-15 20:04:01 +0100

    html/css project with webpack

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const AutoPrefixer = require("autoprefixer");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+
+  /*
+  development mode prevents minification of generated code.
+  devtool none prevents the use of eval and other conventions.
+  contentHash prevents caching of files which can interfere with loading of changes.
+
+  HtmlWepbackPlugin generates html file from template and inserts js and css files.
+  Html-loader and File-loader handle image generation and intergration.
+  MiniCSSExtract Plugin generates stand alone css file.
+  */ 
+
+  mode: "development",
+  devtool: 'none',
+  entry: './src/index.js',
+  target: "node",
+  output: {
+    filename: 'main.[contentHash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },   //end of output
+
+  plugins : [
+
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: '[name].[contentHash].css'
+     }),
+
+     new CleanWebpackPlugin()
+
+  ], //end of plugins
+
+  module: {
+    
+    rules: [
+
+      {
+        test: /\.html$/,
+        loader: ['html-loader'],
+      },
+
+      {
+        test: /\.(svg|png|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: "images"
+              }
+          }
+        ]   //end of file-loader use
+      },
+
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader:'css-loader',
+            options: {
+              sourceMap: true,
+            }
+          },
+        
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              plugins: () => [AutoPrefixer()],
+              config: {
+                path: 'postcss.config.js'
+              }
+            }
+          },
+          'sass-loader'
+        ],
+      }
+    ]   //end of rules
+  }   //end of module
+}   //end of module.exports
